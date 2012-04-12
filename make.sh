84386b4fbef0bf11b532bf0a43a10cd2a13700d7
fet mar <fetmar@gmail.com>
2012-04-12 16:16:56 -0400

    Make file produces APK backups in your home directory
    1 per hour

--- make.sh
+++ make.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 # Edit this file to match your folders
+
+BACKUP="tangerine-apks"
+if [ ! -d "$BACKUP" ]; then
+mkdir $HOME/$BACKUP
+fi
+
+
 echo
 echo "********"
 echo "Cleaning"
 echo "********"
 ant clean
+# ant debug clean
 echo
 echo "********"
 echo "Compacting database"
@@ -25,4 +33,15 @@ echo "**********"
 echo "Installing"
 echo "**********"
 adb install bin/Tangerine-debug.apk
+echo
+echo "**********"
+echo "Backing up to $HOME/$BACKUP"
+echo "**********"
+
+
+NOW=$(date +"%Y%m%d-%H")
+FILE="Tangerine-debug.$NOW.tar.gz"
+tar czfv $FILE bin/Tangerine-debug.apk
+cp -f $FILE $HOME/$BACKUP 
+rm $FILE
 #adb shell am start -n com.couchbase.callback/.AndroidCouchbaseCallback
