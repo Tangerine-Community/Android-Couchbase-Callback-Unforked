5ec4ef22b47c159d4948463bb344bef1afd8e496
fet mar <fetmar@gmail.com>
2012-07-16 22:43:27 -0400

    Section titles for make script

--- make.sh
+++ make.sh
@@ -3,51 +3,60 @@
 
 BACKUP="tangerine-apks"
 if [ ! -d "$BACKUP" ]; then
-mkdir $HOME/$BACKUP
+mkdir $HOME/$BACKUP > /dev/null
 fi
+
+
+echo -e "\n\nTangerine Make Script\n\n"
 
 read -p "Do you want to delete your current database and start with a clean, new empty one?" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 # Clean out the tangerine DB of any existing assessments or results
-curl -H "Content-Type: application/json" -X DELETE http://tangerine:tangytangerine@localhost:5984/tangerine; curl -H "Content-Type: application/json" -X PUT http://tangerine:tangytangerine@localhost:5984/tangerine; cd ../app; couchapp push; cd -
+echo -e "\n********"
+echo      "Deleting local database"
+echo      "********"
+
+curl -H "Content-Type: application/json" -X DELETE http://tangerine:tangytangerine@localhost:5984/class; curl -H "Content-Type: application/json" -X PUT http://tangerine:tangytangerine@localhost:5984/class; cd ../app; 
+
+echo -e "\n*********************"
+echo      "Pushing with CouchApp"
+echo      "*********************"
+
+
+couchapp push; cd -
 fi
 
-echo
-echo "********"
-echo "Cleaning"
-echo "********"
+
+echo -e "\n********"
+echo      "Cleaning"
+echo      "********"
 ant clean
 # ant debug clean
-echo
-echo "********"
-echo "Compacting database"
-echo "********"
-curl -X POST -H "Content-Type: application/json" http://tangerine:tangytangerine@localhost:5984/tangerine/_compact
-echo
-echo "********"
-echo "Building"
-echo "********"
+echo -e "\n********"
+echo      "Compacting database"
+echo      "********"
+curl -X POST -H "Content-Type: application/json" http://tangerine:tangytangerine@localhost:5984/class/_compact
+echo -e "\n********"
+echo      "Building"
+echo      "********"
 ant debug
 NOW=$(date +"%Y%m%d-%H")
-cp bin/Tangerine-debug.apk bin/Tangerine-$NOW.apk
-echo 
-echo "************"
-echo "Uninstalling"
-echo "************"
+cp bin/Tangerine-debug.apk bin/TangerineClass-$NOW.apk
+echo -e "\n************"
+echo      "Uninstalling"
+echo      "************"
 adb uninstall com.couchbase.callback
-echo
-echo "**********"
-echo "Installing"
-echo "**********"
+echo -e "\n**********"
+echo      "Installing"
+echo      "**********"
 adb install bin/Tangerine-debug.apk
-echo
-echo "**********"
-echo "Backing up to $HOME/$BACKUP"
-echo "**********"
+echo -e "\n**********"
+echo      "Backing up to $HOME/$BACKUP"
+echo      "**********"
 
 
-FILE="Tangerine-debug.$NOW.tar.gz"
+FILE="TangerineClass-debug.$NOW.tar.gz"
 tar czfv $FILE bin/Tangerine-debug.apk
 cp -f $FILE $HOME/$BACKUP 
 rm $FILE
